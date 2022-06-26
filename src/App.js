5b3d15958c8201fecafb36de98bc147c2d60755b
immindy4527 <immindy4527@gmail.com>
2022-06-26 17:25:09 +0800

    星星

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import { Rate } from 'antd'
+import 'antd/dist/antd.css'
+const App = () => <Rate disabled defaultValue={4} />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default App
